b7f8ed6b6ca84e2461f9a95f44f05edb6f12f5e8
boomzin windows work <boomzin@gmail.com>
2021-10-08 13:05:04 +0300

    init commit

---
+++ src/main/java/com/example/roomscheduler/RoomschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.roomscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomschedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomschedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/roomscheduler/RoomschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roomscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomschedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
