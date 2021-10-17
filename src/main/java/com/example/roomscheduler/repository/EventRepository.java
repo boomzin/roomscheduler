d0aae6091bd2a817aae61031d7b8f4da2244e229
boomzin windows work <boomzin@gmail.com>
2021-10-17 20:08:23 +0300

    add ManagerEventController

--- src/main/java/com/example/roomscheduler/repository/EventRepository.java
+++ src/main/java/com/example/roomscheduler/repository/EventRepository.java
@@ -15,6 +15,9 @@ public interface EventRepository extends JpaRepository<Event, Integer> {
     @Query("SELECT e FROM Event e WHERE upper(e.duration) > current_timestamp")
     List<Event> getAllActualEvents();
 
+    @Query("SELECT e FROM Event e WHERE lower(e.duration) > current_timestamp AND e.status='STATELESS'")
+    List<Event> getAllActualStatelessEvents();
+
     @Query("SELECT e FROM Event e WHERE upper(e.duration) > current_timestamp AND e.status='CONFIRMED'")
     List<Event> getAllConfirmedActualEvents();
 
@@ -22,7 +25,10 @@ public interface EventRepository extends JpaRepository<Event, Integer> {
     List<Event> getAllActualEventsForUser(int id);
 
     @Query(value = "SELECT e.id FROM event e WHERE e.room_id=:roomId AND e.status='CONFIRMED' AND e.duration && CAST(:duration AS tsrange)", nativeQuery = true)
-    List<Integer> getIntersections(int roomId, String duration);
+    List<Integer> getConfirmedIntersections(int roomId, String duration);
+
+    @Query(value = "SELECT * FROM event e WHERE e.room_id=:roomId AND e.status='CONFIRMED' AND e.duration && CAST(:duration AS tsrange) AND lower(e.duration) > current_timestamp", nativeQuery = true)
+    List<Event> getActualStatelessIntersections(int roomId, String duration);
 
     @Query(value = "SELECT * FROM Event e WHERE e.id = :id and e.user_id = :userId", nativeQuery = true)
     Optional<Event> get(int id, int userId);
@@ -32,4 +38,9 @@ public interface EventRepository extends JpaRepository<Event, Integer> {
                 () -> new IllegalRequestDataException("Event id=" + id + " doesn't belong to User id=" + userId));
     }
 
+    @Query("SELECT e FROM Event e WHERE e.id = :id and e.status = 'STATELESS'")
+    Optional<Event> getStatelessById(int id);
+
+    @Query(value = "SELECT e.room_id FROM Event e WHERE e.id = :id", nativeQuery = true)
+    int getRoomId(int id);
 }
